9f3b7a6ee5c472b25436321623791c6cd70786f1
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2017-09-11 17:53:12 -0600

    Changed permissions on folder

--- axios.php
+++ axios.php
@@ -2,17 +2,19 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
+<title>Axios Form</title>
 
 <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
 <script type="text/javascript" src="node_modules/axios/dist/axios.min.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script>
 	$('document').ready(function(){
 		console.log("jquery is running :D");
 	});
 	</script>
+</head>
+
+<body>
+
 </body>
 </html>

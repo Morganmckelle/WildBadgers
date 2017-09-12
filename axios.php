b619e0a81e7d522aa087cd5c38ac2fd679656899
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2017-09-11 18:32:06 -0600

    ADDED MORGAN'S CODE, THIS PUSH IS HER CODE.

--- axios.php
+++ axios.php
@@ -2,19 +2,17 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Axios Form</title>
+<title>Untitled Document</title>
+</head>
+
+<body>
 
 <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
 <script type="text/javascript" src="node_modules/axios/dist/axios.min.js"></script>
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script>
 	$('document').ready(function(){
 		console.log("jquery is running :D");
 	});
 	</script>
-</head>
-
-<body>
-
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,12 +7,22 @@
 
 <body>
 
-<script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
-<script type="text/javascript" src="node_modules/axios/dist/axios.min.js"></script>
+<script type="text/javascript" src="../../../Users/Chris Hansen/Documents/DGM 3790/p2/node_modules/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="../../../Users/Chris Hansen/Documents/DGM 3790/p2/node_modules/axios/dist/axios.min.js"></script>
 <script>
 	$('document').ready(function(){
 		console.log("jquery is running :D");
 	});
 	</script>
+	
+	<form method="post" action="welcome.php">
+	<input type="text" name="name" />
+	<input type="text" name="from" />
+	<input type="text" name="body" />
+	
+	<input type="submit" />
+	
+	
+	</form>
 </body>
 </html>
